60d3582b15671b20e6ecd557b56afc744ae71436
Waleed Zafar <58841410+waleedzafar4578@users.noreply.github.com>
2025-08-06 11:21:26 +0500

    Add Timer in frontend

--- fochat/src/elements/InputBar.tsx
+++ fochat/src/elements/InputBar.tsx
@@ -2,10 +2,12 @@ import { useContext, useState } from "react";
 import type { ChangeEvent } from "react";
 import './elements.css';
 import { WebSocketContext } from "../context/WsContext";
-
+import Timer from "./Timer";
 function InputBar({ doOnClick }: { doOnClick: () => void }) {
   const syb = ["📤", "🔌"];
   const [smsText, setSmsText] = useState<string>("");
+  const [limiter, setLimter] = useState<boolean>(false);
+  const [sec, setSec] = useState<number>(10);
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setSmsText(e.target.value)
   }
@@ -15,10 +17,10 @@ function InputBar({ doOnClick }: { doOnClick: () => void }) {
     throw new Error("WebSocket Valuse not provided!");
   }
 
-  const { sendMessage, ws,reconnect } = context;
+  const { sendMessage, ws, reconnect } = context;
 
   const sendtoMessage = () => {
-    if(ws ===null){
+    if (ws === null) {
       reconnect()
       return
     }
@@ -26,6 +28,7 @@ function InputBar({ doOnClick }: { doOnClick: () => void }) {
       sendMessage(smsText);
       setSmsText("")
       doOnClick()
+      setLimter(!limiter)
     }
   }
   const handleKeydown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
@@ -37,18 +40,20 @@ function InputBar({ doOnClick }: { doOnClick: () => void }) {
   return (
     <div className="inputbar-container">
       <textarea
-        disabled={ws === null ? true : false}
+        disabled={ws === null || limiter ? true : false}
         value={smsText}
         onChange={handleChange}
         className="input-bar"
         onKeyDown={handleKeydown}
-        placeholder={ws === null ? "Bhai isko connect krly,moo utha kr betha hi" : "Chalo! tum apni luch taalo."}
+        placeholder={ws === null ? "Bhai isko connect krly,moo utha kr betha hi" : limiter ? "Bhai intazar kro,bar bar sms likny  se adat krab ho jati." : "Chalo! tum apni luch taalo."}
       />
       {smsText.length < 100 && (
         <div className="inputbar-button-container" >
           <div onClick={sendtoMessage} className="inputbar-button" style={{
             backgroundColor: ws === null ? "#E14434" : "#fff"
-          }}>{ws === null ? syb[1] : [syb[0]]}</div>
+          }}>{ws === null ? syb[1] : limiter ? <Timer changer={() => {
+            setLimter(!limiter)
+          }} timeLimit={10} outerSec={() => setSec(sec - 1)} /> : [syb[0]]}</div>
         </div>
       )}
       <div className="word-counter" style={{
